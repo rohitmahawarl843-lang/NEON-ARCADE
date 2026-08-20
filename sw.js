028b30c8087117d235ff30a2034883f809957036
rohitmahawarl843-lang <rohitmahawarl843@gmail.com>
2026-08-20 14:24:45 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'neon-arcade-v1';
+const CACHE_NAME = 'neon-arcade-v2';
 const ASSETS = ['./', './index.html', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
